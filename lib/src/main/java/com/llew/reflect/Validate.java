d6e0ad5adf9d590abbfc4eadacb8e695166e8d67
wei.liu <wei.liu@ushow.media>
2018-01-13 19:34:59 +0800

    add reflect lib

---
+++ lib/src/main/java/com/llew/reflect/Validate.java
@@ -0,0 +1,11 @@
+package com.llew.reflect;
+
+
+class Validate {
+
+    static void isTrue(final boolean expression, final String message, final Object... values) {
+        if (expression == false) {
+            throw new IllegalArgumentException(String.format(message, values));
+        }
+    }
+}
